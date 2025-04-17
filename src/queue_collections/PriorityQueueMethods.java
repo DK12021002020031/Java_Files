cec7063c64cf1d85448ccb23a596ac16598ff1f2
UEM <diprit.work@gmail.com>
2025-04-17 16:51:47 +0530

    Day 17 Files Added

---
+++ src/queue_collections/PriorityQueueMethods.java
@@ -0,0 +1,12 @@
+package queue_collections;
+
+import java.util.PriorityQueue;
+
+public class PriorityQueueMethods {
+	
+	public static void main(String[] args) {
+		
+		PriorityQueue pq=new PriorityQueue<>();
+		
+	}
+}
